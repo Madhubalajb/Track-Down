0aa86c24dd78d54273c37a71ec757ff8f180c794
Madhubalajb <jbmadhumng@gmail.com>
2021-01-10 16:37:33 +0530

    Add Track data

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,29 +5,59 @@ import TrackSheet from './TrackSheet'
 import habitService from '../services/habit_services'
 
 const Home = () => {
+    const daysInMonth = {
+        January: 31,
+        February: 28,
+        March: 31,
+        April: 30,
+        May: 31,
+        June: 30,
+        July: 31,
+        August: 31,
+        September: 30,
+        October: 31,
+        November: 30,
+        December: 31 
+    }
     const [show, setShow] = useState(false)
     const [habitName, setHabitName] = useState('') 
     const [habitMonth, setHabitMonth] = useState('')
 
-    const handleHabitName = (event) => setHabitName(event.target.value)
-    const handleHabitMonth = (event) => setHabitMonth(event.target.value)
     const handleClose = () => setShow(false)
     const handleShow = () => setShow(true)
+    const handleHabitName = (event) => setHabitName(event.target.value)
+    const handleHabitMonth = (event) => setHabitMonth(event.target.value)
     
     const makeStateNull = () => {
         setHabitName('')
         setHabitMonth('')
     }
+
+    function handleActivity() {
+        return new Promise((resolve, reject) => {
+            const dummyArray = []
+            for(let i = 1; i <= daysInMonth[habitMonth]; i++) {
+                dummyArray.push(false)
+            }
+            if(dummyArray.length !== 0) {
+                resolve(dummyArray)
+            }
+        })
+    }
     
     const submitHabit = (event) => {
         event.preventDefault()
-        const newHabit = {
-            name: habitName,
-            month: habitMonth
-        }
-        habitService.addData(newHabit)
-        makeStateNull()
-        setShow(false)
+        handleActivity().then(data => {
+            const newHabit = {
+                habit_name: habitName,
+                habit_month: habitMonth,
+                habit_track: data
+            }
+            console.log(newHabit)
+            habitService.addData(newHabit)
+            makeStateNull()
+            setShow(false)
+        }) 
     }
 
     return (
@@ -35,14 +65,14 @@ const Home = () => {
             <center>
                 <Card>
                     <Card.Body>
-                        <p>Add a new habbit to track</p>
+                        <p>Add a new habit to track</p>
                         <center>
                             <i className="material-icons" title="Add Habit" onClick={handleShow}>add_circle</i>
                         </center>
                     </Card.Body>
                 </Card>
                 <HabitForm show={show} close={handleClose} habit_name={handleHabitName} habit_month={handleHabitMonth} submit={submitHabit}/>
-                <TrackSheet monthName={habitMonth} />
+                <TrackSheet monthName={habitMonth} daysInMonth={daysInMonth} />
             </center>
         </Container>
     )
--- src/components/TrackSheet.js
+++ src/components/TrackSheet.js
@@ -3,22 +3,8 @@ import {Card, Container, Button} from 'react-bootstrap'
 
 const TrackSheet = (props) => {
     let days = []
-    const daysInMonth = {
-        January: 31,
-        February: 28,
-        March: 31,
-        April: 30,
-        May: 31,
-        June: 30,
-        July: 31,
-        August: 31,
-        September: 30,
-        October: 31,
-        November: 30,
-        December: 31 
-    }
     
-    for (let i = 1; i <= daysInMonth[props.monthName]; i++) {
+    for (let i = 1; i <= props.daysInMonth[props.monthName]; i++) {
         days.push(<Button className="dayOfMonth" key={i} id={i}>{i}</Button>)
     }
 
